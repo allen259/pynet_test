befdd2c2cadbe84120a750d8164c8f11f80f21fb
Allen Huynh <ahuynh@uber.com>
2017-03-20 16:15:49 -0700

    file lab

---
+++ file-ex1-w.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+f = open("list_ex1_w", "a")
+f.write("Hello this is write file")
+f.close()
---
+++ file_ex1.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+f = open("file_ext.txt")
+output = f.read()
+print output
